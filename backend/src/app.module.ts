25035f04214f55c46b6c9559e73d5c0621aa8332
Kasun Sampath <ks2005755@gmail.com>
2025-08-04 17:50:56 +0530

    post component commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './components/post/post.module';
+import { PrismaService } from './prisma/prisma.service';
 
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
   imports: [PostModule],
+  exports: [PrismaService]       
 })
 export class AppModule {}
--- backend/src/components/post/post.service.ts
+++ backend/src/components/post/post.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(private readonly prisma: PrismaService) {} // Inject PrismaService
+  //create
+  async create(createPostDto: CreatePostDto) {
+    return await this.prisma.post.create({
+      data: createPostDto,
+    });
   }
-
-  findAll() {
-    return `This action returns all post`;
+  //find all
+    async findAll() {
+    return await this.prisma.post.findMany();
+    }
+  //find one
+  async findOne(id: number) {
+    return await this.prisma.post.findUnique({
+      where: { id },
+    });
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  //update
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    return await this.prisma.post.update({
+      where: { id },
+      data: updatePostDto,
+    });
   }
-
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  //remove
+  async remove(id: number) {
+    return await this.prisma.post.delete({
+      where: { id },
+    });
   }
 }
